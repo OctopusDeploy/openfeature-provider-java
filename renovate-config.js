fd09995eda0d53ebb0855201dd1e5a33e343a7de
James Cameron <james.cameron@octopus.com>
2026-07-15 12:36:16 +1000

    ci: add Renovate (#22)

---
+++ renovate-config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  platform: 'github',
+  timezone: 'Australia/Brisbane',
+  requireConfig: 'optional',
+  onboarding: false,
+
+  repositories: ['OctopusDeploy/openfeature-provider-java'],
+  reviewers: ['team:team-devex'],
+  branchPrefix: 'renovate/',
+
+  // Limit concurrent PRs for better manageability
+  prConcurrentLimit: 5,
+
+  // Add labels to PRs
+  labels: ['dependencies'],
+
+  // Wait until a release has been published for at least 2 days before updating,
+  // to avoid picking up versions that get yanked or hotfixed shortly after release.
+  minimumReleaseAge: '2 days',
+
+  // Enable vulnerability alerts
+  osvVulnerabilityAlerts: true,
+
+  // Produce conventional-commit PR titles so validate-pr-title passes
+  semanticCommits: 'enabled',
+
+  // Extend with recommended config
+  extends: ['config:recommended'],
+
+  // Only manage Maven deps and GitHub Actions (leaves the `specification` submodule alone)
+  enabledManagers: ['maven', 'github-actions'],
+
+  packageRules: [
+    {
+      // GitHub Actions: pin third-party actions to a commit SHA for security.
+      matchManagers: ['github-actions'],
+      matchPackageNames: [
+        '!actions/**', // Trust official GitHub actions (checkout, setup-java, etc.); don't pin.
+      ],
+      pinDigests: true,
+    },
+  ],
+};
